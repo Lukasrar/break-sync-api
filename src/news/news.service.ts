1dbad443148307b74e8edd858be29fc11eed5d21
Lukasrar <lukasrar001@gmail.com>
2024-11-22 18:31:03 -0300

    adding news by profession and cleaning and refetch when day changes

--- src/news/news.service.ts
+++ src/news/news.service.ts
@@ -1,58 +1,91 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { News } from './news.schema';
 import { Model } from 'mongoose';
 import { Cron } from '@nestjs/schedule';
 import axios from 'axios';
+import { DailyNews } from './daily-news.schema';
+import { AllNews } from './all-news.schema';
+import { Profession } from 'src/profession/profession.schema';
 
 @Injectable()
 export class NewsService {
   private readonly logger = new Logger(NewsService.name);
 
-  constructor(@InjectModel(News.name) private newsModel: Model<News>) {}
+  constructor(
+    @InjectModel(DailyNews.name) private dailyNewsModel: Model<DailyNews>,
+    @InjectModel(AllNews.name) private allNewsModel: Model<AllNews>,
+    @InjectModel(Profession.name) private professionModel: Model<Profession>,
+  ) {}
 
-  async create(newsData: Partial<News>): Promise<void> {
-    console.log(newsData);
+  async testCron(): Promise<void> {
     await this.handleCronJob();
   }
 
-  async fetchAndStoreDevToPosts(): Promise<void> {
-    const url = 'https://dev.to/api/articles?page=1&per_page=5';
+  async fetchAndStorePosts(profession): Promise<any[]> {
+    const url = `https://dev.to/api/articles?page=1&per_page=5`;
 
     try {
       this.logger.log('Fetching posts from Dev.to...');
+      this.logger.log(url);
 
-      const response = await axios.get(url);
+      const response = await axios.get(url, { params: profession.tags });
       const articles = response.data;
 
       const curatedArticles = articles.filter(
-        (article) => article.positive_reactions_count > 50,
+        (article) => article.positive_reactions_count > 10,
       );
 
       console.log(curatedArticles);
+      this.logger.log('Posts successfully stored in the database.');
 
-      const formattedArticles = curatedArticles.map((article: any) => ({
+      return articles.map((article: any) => ({
         title: article.title,
-        validUntil: new Date(article.published_at),
+        validUntil: new Date(),
         description: article.description || '',
         readingTimeMinutes: article.reading_time_minutes,
         tagList: article.tag_list || [],
         authorName: article.user.name,
         id: article.id,
+        profession: profession.name,
       }));
-
-      await this.newsModel.insertMany(formattedArticles);
-      this.logger.log('Posts successfully stored in the database.');
     } catch (error) {
       this.logger.error(
         'Failed to fetch or store posts from Dev.to',
         error.message,
       );
+      return [];
     }
   }
 
   @Cron('0 6 * * *')
   async handleCronJob(): Promise<void> {
-    await this.fetchAndStoreDevToPosts();
+    this.logger.log('Starting daily news cron job...');
+
+    const oldNews = await this.dailyNewsModel.find().exec();
+
+    if (oldNews.length > 0) {
+      await this.allNewsModel.insertMany(oldNews);
+      await this.dailyNewsModel.deleteMany({});
+      this.logger.log('Migrated daily news to allNews and cleared dailyNews.');
+    }
+
+    const professions = await this.professionModel.find().exec();
+    this.logger.log(professions);
+
+    for (const profession of professions) {
+      const newsForProfession = await this.fetchAndStorePosts(profession);
+
+      if (newsForProfession.length > 0) {
+        await this.dailyNewsModel.insertMany(newsForProfession);
+        this.logger.log(
+          `Stored ${newsForProfession.length} articles for profession: ${profession.name}`,
+        );
+      }
+    }
+    this.logger.log('Daily news cron job completed.');
+  }
+
+  async getNewsByProfession(profession: string): Promise<any[]> {
+    return await this.dailyNewsModel.find({ profession }).exec();
   }
 }
